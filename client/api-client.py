1f7d3edbee47e8b84b6f277bc54df924ab9bf296
FaisalQ07 <qureshi.faisal712@gmail.com>
2024-01-08 18:02:41 -0500

    Modified the API client to post category and product to API-Ingest by looping through the dataset/output files, amazon_categories.txt and amazon_products_small. Tested successfully

--- client/api-client.py
+++ client/api-client.py
@@ -1,35 +1,44 @@
+import os
 import linecache
 import json
-
+import glob
 # Make sure that requests is installed in your WSL
 import requests 
 
 # We could just read the entire file, but if it's really big you could go line by line
 # If you want make this an excercise and replace the process below by reading the whole file at once and going line by line
 
-#set starting id and ending id
-start = 1
-end = 50
-
 # Loop over the JSON file
-i=start
 
-while i <= end:     
-    
-    # read a specific line
-    line = linecache.getline('./output.txt', i)
-    #print(line)
-    # write the line to the API
-    myjson = json.loads(line)
-    
-    print(myjson)
-    
-    response = requests.post('http://localhost:80/invoiceitem', json=myjson)
+print('here!')
+for filepath in glob.iglob('../dataset/output/*.txt'):
+    print(filepath)
+    head, tail = os.path.split(filepath)
+    filename = tail.split('.')[0]
+    #set starting id and ending id
+    start = 11
+    end = 20
+    i=start
+    while i < end:
+        line = linecache.getline(filepath, i)
+        # print(line)
+        # write the line to the API
+        try:
+            myjson = json.loads(line)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in line {i}: {e}")
+            # handle the error or skip the line if necessary
+            continue
+        print(myjson)
+        if filename == 'amazon_products_small':
+            response = requests.post('http://localhost:80/AmazonProduct', json=myjson)
+        elif filename == 'amazon_categories':
+            response = requests.post('http://localhost:80/Category', json=myjson)
 
-    # Use this for dedbugging
-    #print("Status code: ", response.status_code)
-    #print("Printing Entire Post Request")
-    print(response.json())
-
-    # increase i
-    i+=1
+        try:
+            response_json = response.json()
+            print(response_json)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in response: {e}")
+        # increase i
+        i+=1
